c415cbcb97a2e260e9ce9b8db5535f42ebe0aa89
astrosuka <yegresvoselok@gmail.com>
2025-07-29 21:09:54 -0300

    Display images and use them in background that changes on mouse move

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,60 @@
+import { useState, useEffect } from "react";
+import { getImages } from "./sanity/sanity-utils";
+import type { Image } from "./types/Image";
+
 function App() {
+  const [images, setImages] = useState<Image[] | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getImages();
+      setImages(data);
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const { clientX, clientY } = e;
+    const newPosition = {
+      x: (clientX / window.innerWidth) * 100,
+      y: (clientY / window.innerHeight) * 100,
+    };
+    setMousePosition(newPosition);
+  };
+
+  if (isLoading) return <div>...</div>;
+
   return (
     <>
-      <div className="w-fit p-4 text-xl">agujajajajaa</div>
+      <div
+        className="fixed top-0 left-0 h-screen w-screen cursor-crosshair overflow-hidden bg-center opacity-100"
+        style={{
+          backgroundImage: `url(${
+            (images &&
+              images[Math.floor(Math.random() * images.length)].file.asset.url +
+                "?fm=webp") ||
+            ""
+          })`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "100% 100%",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <div className="relative opacity-95 mix-blend-color-burn">
+          <div className="absolute top-0 right-0 bottom-0 left-0 h-screen w-[150%] bg-black/70"></div>
+          <div
+            onMouseMove={handleMouseMove}
+            className="absolute top-0 right-0 bottom-0 left-0 h-screen w-full bg-[#fff]"
+            style={{
+              maskImage: `radial-gradient(ellipse 600px 600px at ${mousePosition.x}% ${mousePosition.y}%, red 10%, transparent 90%)`,
+            }}
+          ></div>
+        </div>
+      </div>
     </>
   );
 }
