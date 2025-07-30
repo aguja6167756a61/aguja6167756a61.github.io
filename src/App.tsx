b02dc2f5066e0ee7040b56faeb729cb6bfe098d1
astrosuka <yegresvoselok@gmail.com>
2025-07-29 23:30:42 -0300

    Add links

--- src/App.tsx
+++ src/App.tsx
@@ -112,6 +112,44 @@ function App() {
           )}
         </div>
 
+        {/* <button */}
+        {/*   onClick={() => setCreditsOpen(!creditsOpen)} */}
+        {/*   className="z-[200] pb-1 leading-none sm:hover:bg-[#f00]" */}
+        {/* > */}
+        {/*   (?) */}
+        {/* </button> */}
+
+        <ul className="fixed top-4 right-4 flex rotate-180 gap-4 text-xs uppercase [writing-mode:vertical-lr]">
+          <li className="-ml-1 pl-1 leading-none sm:hover:bg-[#f00]">
+            <a
+              target="_blank"
+              href="https://www.ninaprotocol.com/releases/aguja-m-u-sica-electr-onica-i"
+            >
+              nina
+            </a>
+          </li>
+          <li className="-ml-1 pl-1 leading-none sm:hover:bg-[#f00]">
+            <a
+              target="_blank"
+              href="https://unun.bandcamp.com/album/un002-m-sica-electr-nica-i"
+            >
+              bandcamp
+            </a>
+          </li>
+          <li className="-ml-1 pl-1 leading-none sm:hover:bg-[#f00]">
+            <a target="_blank" href="https://soundcloud.com/aguja6167756a61">
+              soundcloud
+            </a>
+          </li>
+          <li className="-ml-1 pl-1 leading-none sm:hover:bg-[#f00]">
+            <a
+              target="_blank"
+              href="https://www.instagram.com/aguja6167756a61/"
+            >
+              instagram
+            </a>
+          </li>
+        </ul>
       </div>
 
       <Player embed={SOUNDCLOUD_EMBED} />
